4b1ab2972b3fcbe3c05829203dcb31a34ab6608a
Felipe Lima <felipenplima@gmail.com>
2020-03-04 15:06:31 +0000

    2

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,35 @@ import React, { Component } from 'react';
 import './App.scss';
 
 import meals from './meals.json';
-
 import MealBox from './Component/MealBox';
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      meals: meals
+      meals: meals,
+      showFrom: false,
+      search: '',
+      arr: []
     };
+    this.renderForm = this.renderForm.bind(this);
+  }
+
+  renderForm() {
+    this.setState({
+      showForm: !this.state.showForm
+    });
   }
 
   render() {
-    return <div>{<MealBox />}</div>;
+    return (
+      //the array of meals is being rendered here
+      <div className="App">
+        {this.state.meals.map(meals => {
+          return <MealBox {...meals} />;
+        })}
+      </div>
+    );
   }
 }
 
--- src/Component/MealBox/index.jsx
+++ src/Component/MealBox/index.jsx
@@ -4,32 +4,25 @@ import './styles.css';
 class MealBox extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      //why?
-      count: 0
-    };
+    this.state = {};
   }
 
   render() {
     return (
       <div className="media">
+        <img></img>
         <img
           src="https://i.imgur.com/eTmWoAN.png"
-          className="img-thumbnail mr-3 mw-25 border-0"
-          style={{ maxWidth: '10em' }}
+          class="img-thumbnail mr-3 mw-25 border-0"
+          style={{ maxWidth: '10em' }} //style="max-width: 10em;"
         />
-        <div className="media-body align-self-center">
-          <h5 className="mt-0 mb-1">Pizza</h5>
+        <div class="media-body align-self-center">
+          <h5 class="mt-0 mb-1">Pizza</h5>
           <small>400 cal</small>
         </div>
-        <form className="row align-self-center">
-          <input
-            className="form-control col-9"
-            type="number"
-            value="1"
-            onChange={this.controlCount}
-          />
-          <button className="btn btn-primary col-3">+</button>
+        <form class="row align-self-center">
+          <input class="form-control col-9" type="number" value="1" />
+          <button class="btn btn-primary col-3">+</button>
         </form>
       </div>
     );
